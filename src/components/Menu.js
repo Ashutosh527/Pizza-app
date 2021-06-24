fdb37bca738a70adf6daf15564682cb8ea3dc556
Ashutosh527 <61248175+Ashutosh527@users.noreply.github.com>
2021-06-24 19:25:39 +0530

    Add files via upload

---
+++ src/components/Menu.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div className="about__img">
+            <img src="http://i.huffpost.com/gen/1554236/images/o-XBOX-PIZZA-HUT-facebook.jpg" alt="Pizza"/>
+              
+              
+            </div>
+          </div>
+          <div className="col-6 p-25">
+            <h3>The Pizza Menu</h3>
+            <h1>CHICAGO THIN CRUST</h1>
+            <p>
+              end all your hunger;
+            </p>
+            <div className="about__btn">
+              <a href="" className="btn btn-smart">
+                VIEW MORE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar__text">
+        <div className="logo">
+          <img src="https://www.bing.com/th?id=OIP.SD-iixKdVrhVM36ST-lEDwHaFj&w=128&h=100&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" logo/>
+        </div>
+        <ul className="navbar__ul">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">Shop</a>
+          </li>
+          <li>
+            <a href="">News</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
